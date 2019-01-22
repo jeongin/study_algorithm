de79a82fd53b432ca57ea4bacf8325cb92d90979
JEONGIN KIM <uugg2922@gmail.com>
2019-01-22 20:12:50 +0900

    add baekjoon 1005

--- algorithm/problem.cpp
+++ algorithm/problem.cpp
@@ -1,9 +1,37 @@
 #include <iostream>
+#include <map>
 
 using namespace std;
 
+int delay[100001];
+
 int main()
 {
+	// baekjoon 1005
+
+	int T, N, K;
+	int X, Y;
+	map<int, int> relation;
+	int W;
+
+	for (int t = 0; t < T; t++)
+	{
+		cin >> N >> K;
+
+		for (int n = 1; n <= N; n++)
+		{
+			cin >> delay[n];
+		}
+
+		for (int k = 0; k <= K; k++)
+		{
+			cin >> X >> Y;
+			relation.insert(make_pair(X, Y));
+		}
+
+		cin >> W;
+
+	}
 
 
 
