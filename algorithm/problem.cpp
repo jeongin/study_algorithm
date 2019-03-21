5f3611c1a0ac837a8236debe7f7d3bcda52e781d
정인 김 <uugg2922@gmail.com>
2019-03-21 18:23:14 +0900

    solve baekjoon 13458

--- algorithm/problem.cpp
+++ algorithm/problem.cpp
@@ -12,35 +12,7 @@ int B, C;
 int main()
 {
 	ios::sync_with_stdio(false);
-	// baekjoon 13458
-
-	cin >> N;
-	for (int i = 0; i < N; i++)
-	{
-		cin >> A[i];
-	}
-
-	cin >> B >> C;
-
-	long long ans = 0;
-
-	for (int i = 0; i < N; i++)
-	{
-		if (A[i] <= B)
-		{
-			ans += 1;
-		}
-		else if ((A[i] - B) % C == 0)
-		{
-			ans += ((A[i] - B) / C) + 1;
-		}
-		else
-		{
-			ans += ((A[i] - B) / C) + 2;
-		}
-	}
-	
-	cout << ans << "\n";
+	// baekjoon 
 
 	getchar();
 	getchar();
