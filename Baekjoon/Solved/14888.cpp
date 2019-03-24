4d069a410b8284020d406a2cf09cb43a577fcdb0
JeongIn_desktop <JeongIn@gmail.com>
2019-03-24 23:16:15 +0900

    solve baekjoon 14888 add baekjoon 14889

---
+++ Baekjoon/Solved/14888.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int N;
+int num[100];
+int op[4];
+vector<int> ans;
+
+int compare(int a, int b)
+{
+	return (a > b);
+}
+
+int cal(int a, int b, int n, int cnt)
+{
+	if (op[n] == 0)
+		return 0;
+
+	op[n]--;
+
+	int result;
+	if (n == 0)
+	{
+		result = a + b;
+	}
+	else if (n == 1)
+	{
+		result = a - b;
+	}
+	else if (n == 2)
+	{
+		result = a * b;
+	}
+	else
+	{
+		if (b < 0)
+		{
+			result = (a / ((-1) * b)) * (-1);
+		}
+		else
+		{
+			result = a / b;
+		}
+	}
+
+	if (cnt == N - 1)
+	{
+		ans.push_back(result);
+		op[n]++;
+		return 0;
+	}
+
+	for (int i = 0; i < 4; i++)
+	{
+		cal(result, num[cnt + 1], i, cnt + 1);
+	}
+
+	op[n]++;
+	return 0;
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	// baekjoon 14888
+
+	cin >> N;
+
+	for (int i = 0; i < N; i++)
+	{
+		cin >> num[i];
+	}
+
+	for (int i = 0; i < 4; i++)
+	{
+		cin >> op[i];
+	}
+
+	for (int i = 0; i < 4; i++)
+	{
+		cal(num[0], num[1], i, 1);
+	}
+
+	sort(ans.begin(), ans.end(), compare);
+
+	cout << ans[0] << "\n";
+	cout << ans[ans.size() - 1] << "\n";
+
+
+	return 0;
+}
--- algorithm/problem.cpp
+++ algorithm/problem.cpp
@@ -3,87 +3,16 @@
 #include <vector>
 #include <queue>
 
-int N;
-int num[100];
-int op[4];
-vector<int> ans;
-
 using namespace std;
-
-int cal(int a, int b,  int n, int cnt)
-{
-	if (op[n] == 0)
-		return 0;
-
-	op[n]--;
-
-	int result;
-	if (n == 0)
-	{
-		result = a + b;
-	}
-	else if (n == 1)
-	{
-		result = a - b;
-	}
-	else if (n == 2)
-	{
-		result = a * b;
-	}
-	else
-	{
-		if (b < 0)
-		{
-			result = (a / ((-1) * b)) * (-1);
-		}
-		else
-		{
-			result = a / b;
-		}
-	}
-
-	op[n]++;
-	if (cnt == N - 1)
-	{
-		ans.push_back(result);
-		return 0;
-	}
-
-	for (int i = 0; i < 4; i++)
-	{
-		cal(result, num[cnt + 1], i, cnt + 1);
-	}
-
-	return 0;
-}
 
 int main()
 {
 	ios::sync_with_stdio(false);
-	// baekjoon 14888
+	// baekjoon 14889
 
-	cin >> N;
-
-	for (int i = 0; i < N; i++)
-	{
-		cin >> num[i];
-	}
-
-	for (int i = 0; i < 4; i++)
-	{
-		cin >> op[i];
-	}
-
-	for (int i = 0; i < 4; i++)
-	{
-		cal(num[0], num[1], i, 1);
-	}
-
-	sort(ans.begin(), ans.end());
-
-	cout << ans[0] << "\n";
-	cout << ans[ans.size() - 1] << "\n";
-
+	
+	getchar();
+	getchar();
 
 	return 0;
 }
